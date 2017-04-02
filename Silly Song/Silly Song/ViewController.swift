e648dd922bef0ceba2b5b0f1d5f4dc379d4d9970
lgtout <lagos.tout@gmail.com>
2017-04-02 14:05:26 -0700

    Silly Song app - add polish
    Configure text field to auto-capitalize words.
    Return key should say "Done", instead of "Return.
    Reposition lyrics field and update violated constraints.

--- Silly Song/Silly Song/ViewController.swift
+++ Silly Song/Silly Song/ViewController.swift
@@ -26,19 +26,17 @@ class ViewController: UIViewController {
         nameField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func reset(_ sender: Any) {
         nameField.text = nil
         lyricsView.text = nil
     }
 
     @IBAction func displayLyrics(_ sender: Any) {
+        guard let name = nameField.text, !name.isEmpty else {
+            return
+        }
         lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate,
-                                        fullName: nameField.text!)
+                                        fullName: name)
     }
     
 }
